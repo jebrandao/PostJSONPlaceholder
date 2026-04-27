fed4e24f47043b5b7ead45f457d19c6fc6b30d32
JeBrandao <jebrandao@firjan.com.br>
2026-04-27 15:41:00 -0300

    Post em JSON Place Holder

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,68 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
+import { TarefaService } from './services/tarefas';
+import { Tarefa } from './models/tarefas';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cadastro-tarefas');
+
+  private tarefaService = inject(TarefaService);
+
+  loading = signal(false);
+  sucesso = signal('');
+  erro = signal('');
+  tarefaCriada = signal<Tarefa | null>(null);
+
+  form = new FormGroup({
+    titulo: new FormControl('', Validators.required),
+    descricao: new FormControl('', Validators.required),
+    prioridade: new FormControl('media', Validators.required),
+    categoria: new FormControl('', Validators.required),
+    status: new FormControl('pendente', Validators.required),
+    dataLimite: new FormControl('', Validators.required)
+  });
+
+  salvar() {
+    if (this.form.invalid) {
+      this.erro.set('Preencha todos os campos.');
+      return;
+    }
+
+    this.loading.set(true);
+    this.erro.set('');
+    this.sucesso.set('');
+    this.tarefaCriada.set(null);
+
+    const payload = this.form.getRawValue() as Tarefa;
+
+    this.tarefaService.criar(payload).subscribe({
+      next: (resposta) => {
+        this.loading.set(false);
+
+        this.sucesso.set('Tarefa cadastrada com sucesso!');
+        this.tarefaCriada.set(resposta);
+
+        this.form.reset({
+          prioridade: 'media',
+          status: 'pendente'
+        });
+      },
+
+      error: () => {
+        this.loading.set(false);
+        this.erro.set('Erro ao salvar tarefa.');
+      }
+    });
+  }
 }
---
+++ src/app/models/tarefas.ts
@@ -0,0 +1,9 @@
+export interface Tarefa {
+  id?: number;
+  titulo: string;
+  descricao: string;
+  prioridade: string;
+  categoria: string;
+  status: string;
+  dataLimite: string;
+}
